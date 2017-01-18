54db467ad6e6c969de60eeca3c3655999e8f21e1
Sean Pianka <pianka@eml.cc>
2017-01-17 23:08:16 -0500

    nicer popup, selecting students, saving/loading/clearing list

--- background.js
+++ background.js
@@ -1,30 +1,50 @@
-function select_students() {
-    chrome.tabs.executeScript(null, {
-        file: "content.js"
-    });
+function use_jquery(tabId, func) 
+{
+    chrome.tabs.executeScript(tabId, { file: 'jquery-3.1.1.min.js'}, func );
+}
+
+function select_students() 
+{
+    use_jquery(chrome.tabs.executeScript(null, {
+            file: "content.js"
+        },
+        function() {
+            chrome.runtime.sendMessage("Content script run.");
+        })
+    );
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+    student_list_form = document.getElementById('student-list');
+
     document.getElementById('select-students-btn').addEventListener('click', function() {
-        student_list = document.getElementById('student-list').value.split('\n').map(function(str) { 
-            return str.split(',');
+        student_list = student_list_form.value.split('\n').map(function(str) { return str.split(','); });
+
+        if (student_list[0][0] !== "") { action = 1; }
+        else { action = student_list = 0; }
+        chrome.storage.local.set({ 'student_list': student_list, 'action': action}, function() {
+            if (student_list) { select_students(); }
         });
+    });
 
-        console.log(student_list);
+    document.getElementById('clear-list-btn').addEventListener('click', function() {
+        student_list_form.value = ""
+    });
 
-        chrome.storage.sync.set(
-            {
-                'student_list': student_list,
-                'action': 1
-            },
-            function() {
-                chrome.runtime.sendMessage('Student list updated.');
-            }
-        );
+    document.getElementById('load-list-btn').addEventListener('click', function() {
+        chrome.storage.local.get('student_list_text', function(val) {
+            student_list_text = val['student_list_text'];
 
-        select_students();
+            if (student_list_text) { student_list_form.value = student_list_text; }
+        });
+    });
+
+    document.getElementById('save-list-btn').addEventListener('click', function() {
+        if (student_list_form.value)
+        {
+            chrome.storage.local.set({'student_list_text': student_list_form.value}, function() {
+                chrome.runtime.sendMessage('List saved.'); 
+            });
+        }
     });
 });
-
-
-
--- content.js
+++ content.js
@@ -1,14 +1,70 @@
-var action = chrome.storage.sync.get('action');
-
-if (action === 1)
+/*
+ * An attempt replicate Python's str.title.
+ */
+function titlecase(str)
 {
-    var simpleList = document.getElementById('simpleList');
-    var student_list = chrome.storage.sync.get('student_list');
-
-
-    console.log(student_list);
-
-    chrome.storage.sync.set({'action': 0}, function() { 
-        chrome.runtime.sendMessage('Action completed, set to zero.'); 
-    }); // done
+    return str.toLowerCase().split(' ').map(i => i[0].toUpperCase() + i.substring(1)).join(' ');
 }
+
+/*
+ * 'action' keeps track of what operation should be performed. This is a hacky
+ * workaround for my lack of understanding about how to use content script
+ * functions, along with calling them from the background scripts (and passing
+ * the necessary arguments).
+ */
+chrome.storage.local.get('action', function(val) {
+    var action = val['action'];
+
+    if (!action)
+    {
+        /*
+         * Fall-back for no selected action.
+         */
+        console.log("No action was selected.");
+    }
+    else if (action === 1)
+    {                 
+        /*
+         * Select the Students, given by the 'student_list' variable in chrome's
+         * local storage, within the form under the 'Section Criteria' area of the
+         * 'Create Smart View' section of the Smart View creation page.
+         */
+        chrome.storage.local.get('student_list', function(val) {
+            var count = 0;
+            var select_values = [];
+            var simpleList = document.getElementById('simpleList');
+            var student_list = val['student_list'].map(function(str) { 
+                return str.map(function(s) {
+                    return titlecase(s);
+                }).join();
+            });
+
+            $("#simpleList > option").each(function() {
+                name = this.text.split(',').map(function(s) { return titlecase(s); }).join();
+                console.log(name);
+                if (student_list.indexOf(name) >= 0)
+                {
+                    select_values.push(simpleList[count].value);
+                }
+                count++;
+            });
+            $('#simpleList').val(select_values);
+        });
+    }
+    else
+    {
+        /*
+         * Fall-back for a non-zero, unsupported selected action.
+         */
+        console.log("The selected action has no corresponding functionality.");
+    }
+});
+
+
+/*
+ * Reset action to zero for ensuring future actions are uniquely and
+ * purposefully selected.
+ */
+chrome.storage.local.set({'action': 0}, function() { 
+    chrome.runtime.sendMessage('Action completed, set to zero.'); 
+});
